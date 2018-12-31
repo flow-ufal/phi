2ad30982849bac56e461b73c4cf4f22a02d0671c
Augusto José <augusto.alencar@ctec.ufal.br>
2018-12-30 22:09:43 -0300

    Change project path string to getcwd

--- load_cvs.py
+++ load_cvs.py
@@ -2,7 +2,7 @@ import sys
 import os
 import django
 
-sys.path.append("D:/Files/PyCharm Projects/phi")
+sys.path.append(os.getcwd())
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "phi.settings")
 django.setup()
 
